d187e23085bcb507a5ac99a3c29c6f9ee54ea55b
mzzroboto <mzzroboto@users.noreply.github.com>
2018-08-07 21:19:45 +0200

    added responsive navbar

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+
+/*navbar*/
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}

b3fc19f2341505152a6e44476117b7b918ccab81
Rachel <mzzroboto@users.noreply.github.com>
2020-05-29 14:25:24 +0200

    added content type connection to blog

--- me/js/contentful.js
+++ me/js/contentful.js
@@ -1,11 +1,11 @@
 
-const contentful = require('contentful')
 
 const client = contentful.createClient({
   space: 'sgo78rsjtx10',
+  environment: '<environment_id>', // defaults to 'master' if not set
   accessToken: 'nQ2cSFY0WimXrAPCoWTcNve_QsQ8HyksphVSRPMcFj4'
 })
-//Get the blog content type
-client.getContentType('<nQ2cSFY0WimXrAPCoWTcNve_QsQ8HyksphVSRPMcFj4')
+
+client.getContentType('blogPost')
 .then((contentType) => console.log(contentType))
 .catch(console.error)

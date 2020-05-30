9f60a79a8b0a381ecb7717d1399428d1ee86c4f8
Rachel <mzzroboto@users.noreply.github.com>
2020-05-30 14:45:37 +0200

    test

--- me/js/contentful.js
+++ me/js/contentful.js
@@ -1,11 +1,14 @@
 
 
+const contentful = require("contentful");
 const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
   space: 'sgo78rsjtx10',
-  environment: '<environment_id>', // defaults to 'master' if not set
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
   accessToken: 'nQ2cSFY0WimXrAPCoWTcNve_QsQ8HyksphVSRPMcFj4'
-})
-
-client.getContentType('blogPost')
-.then((contentType) => console.log(contentType))
-.catch(console.error)
+});
+// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
+client
+  .getEntry("31TNnjHlfaGUoMOwU0M2og")
+  .then(entry => console.log(entry))
+  .catch(err => console.log(err));

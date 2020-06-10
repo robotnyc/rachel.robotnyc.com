9d75ec6b4483da9f9e79965510e62b0bde5ce9fd
Rachel <mzzroboto@users.noreply.github.com>
2020-06-10 15:44:24 +0200

    made all elements point to contentful and used generic names

--- js/contentful.js
+++ js/contentful.js
@@ -29,18 +29,59 @@ imageSetSourceFromAsset('imageHeader','3UUXMit9S3rNHArXhVd4kl');
 //Set image footer
 imageSetSourceFromAsset('imageFooter','23gSJXQevR0HyatcMqbE2W');
 
+//Get Services
+client.getEntry('67Ccw8vvJub28j1o6ZfXkm')
+.then(function (entry) {
+  if (entry.fields.body) {
+    console.log(entry);
+document.getElementById("Service1Title").insertAdjacentHTML('afterbegin',entry.fields.name);
+document.getElementById("Service1Body").insertAdjacentHTML('afterbegin',entry.fields.body);
+
+  }
+})
+.catch(console.error)
 
 
+client.getEntry('1zicvYgMf6LAWtbsUoi5rD')
+.then(function (entry) {
+  if (entry.fields.body) {
+    console.log(entry);
+    document.getElementById("Service2Title").insertAdjacentHTML('afterbegin',entry.fields.name);
+    document.getElementById("Service2Body").insertAdjacentHTML('afterbegin',entry.fields.body);
+
+  }
+})
+.catch(console.error)
 
 
-//Get Software Implementation entry
+client.getEntry('31MGMZ9cOEyq1PQkbyXdb')
+.then(function (entry) {
+  if (entry.fields.body) {
+    console.log(entry);
+    document.getElementById("Service3Title").insertAdjacentHTML('afterbegin',entry.fields.name);
+    document.getElementById("Service3Body").insertAdjacentHTML('afterbegin',entry.fields.body);
+
+  }
+})
+.catch(console.error)
+
+
+client.getEntry('73qFxyg0eEJemMBjDdR4je')
+.then(function (entry) {
+  if (entry.fields.body) {
+    console.log(entry);
+    document.getElementById("Service4Title").insertAdjacentHTML('afterbegin',entry.fields.name);
+    document.getElementById("Service4Body").insertAdjacentHTML('afterbegin',entry.fields.body);
+  }
+})
+.catch(console.error)
+
+//Get About Me
 client.getEntry('1xT2SUdopy1JCedFJ563S1')
 .then(function (entry) {
   if (entry.fields.body) {
     console.log(entry);
-    //Get Services
-    document.getElementById("technicalPM").insertAdjacentHTML('afterbegin',entry.fields.body);
-    //Get About Me info
+
     document.getElementById("aboutMeLeft").insertAdjacentHTML('afterbegin',entry.fields.body);
     document.getElementById("aboutMeRight").insertAdjacentHTML('afterbegin',entry.fields.body);
     
